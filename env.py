ade421c33947ae1032f39a42087387856c0cda8c
Andrew croshaw <120042631+A-Croshaw@users.noreply.github.com>
2024-08-07 20:46:56 +0100

    inital setup

---
+++ env.py
@@ -0,0 +1,6 @@
+import os
+
+os.environ["DATABASE_URL"] = "postgres://CLICK_TO:REVEAL_PASSWORD@mun-manager2024-menu-manager.i.aivencloud.com:19690/defaultdb?sslmode=require"
+os.environ["SECRET_KEY"] = "whXEyw6BpCDmUJpqylrLGOWX91h4D8p8"
+os.environ["CLOUDINARY_URL"] = "cloudinary://997364773977895:136GXYPU8owfnUvWyd54XHhbA0U@dyqyyqben"
+os.environ["DEVELOPMENT"] = "True"

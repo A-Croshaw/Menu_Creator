dacd7b8b6701907a41a606fb666cb982dd838d8f
Andrew croshaw <120042631+A-Croshaw@users.noreply.github.com>
2024-08-14 04:09:04 +0100

    Added Add Edit And Delete Functions and pages for products

--- products/urls.py
+++ products/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.products_home, name='products'),
     path('all/', views.all_products, name='products_all'),
+    path('add/', views.add_product, name='product_add'),
+    path('edit/<int:product_id>/', views.edit_product, name='product_edit'),
+    path('delete/<int:product_id>/', views.product_delete, name='product_delete'),
 ]
--- products/views.py
+++ products/views.py
@@ -3,12 +3,14 @@ from django.contrib import messages
 from django.db.models import Q
 from django.db.models.functions import Lower
 from .models import Product, Category, Subcategory
+from .forms import ProductForm
 
 def products_home(request):
     """
     A view to render products manager
     """
     return render(request, 'products/products_home.html')
+
 
 def all_products(request):
     """
@@ -32,7 +34,7 @@ def all_products(request):
             if sortkey == 'category':
                 sortkey = 'category__category'
             if sortkey == 'subcategory':
-                sortkey = 'subcategory__category'
+                sortkey = 'subcategory__subcategory'
             if 'direction' in request.GET:
                 direction = request.GET['direction']
                 if direction == 'desc':
@@ -68,3 +70,81 @@ def all_products(request):
     }
 
     return render(request, 'products/products_all.html', context)
+
+
+def add_product(request):
+    """ Adding products, (admin users only) """
+    #if not request.user.is_superuser:
+       # messages.error(request, 'Admin users can only add products.')
+        #return redirect(reverse('home'))
+
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            product = form.save()
+            messages.success(request, f'{product.name} was successfully added')
+            return redirect(reverse('products_all'))
+        else:
+            messages.error(request,
+                           f'failed to add {product.name}.'
+                           'Please ensure the details are correct and'
+                           'all fields that are required are entered.'
+                           )
+    else:
+        form = ProductForm()
+
+    template = 'products/product_add.html'
+    context = {
+        'form': form,
+    }
+    return render(request, template, context)
+
+
+def edit_product(request, product_id):
+    """ Edit a product """
+    #if not request.user.is_superuser:
+    #    messages.error(request, 'Admin users only can edit products.')
+    #    return redirect(reverse('home'))
+
+    product = get_object_or_404(Product, pk=product_id)
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES, instance=product)
+        if form.is_valid():
+            form.save()
+            messages.success(request, f'{product.name} was successfully updated!')
+            return redirect(reverse('products'))
+        else:
+            messages.error(
+                request,
+                f'Updating {product.name} failed.'
+                ' Please ensure the details are valid.')
+    else:
+        form = ProductForm(instance=product)
+        messages.info(request, f'Editing: {product.name}')
+
+    template = 'products/product_edit.html'
+    context = {
+        'form': form,
+        'product': product,
+    }
+
+    return render(request, template, context)
+
+
+def product_delete(request, product_id):
+    """ Delete product """
+    #if not request.user.is_superuser:
+    #    messages.error(request, 'Admin users can only delete products.')
+    #    return redirect(reverse('home'))
+
+    product = get_object_or_404(Product, pk=product_id)
+    if request.method == "POST":
+        product.delete()
+        messages.success(
+            request, f'{product.name} has been delete successfully!')
+        return redirect(reverse('products'))
+
+    context = {
+        'product': product,
+    }
+    return render(request, "products/product_delete.html", context)
